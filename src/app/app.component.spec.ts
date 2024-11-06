4cbc42b6be55abdb663c18f3306149d3a06b400e
Sylwia <sylwiaszymanska2306@gmail.com>
2024-11-06 15:42:43 +0100

    best quotes starter aplication

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,20 +12,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bestQuotes'`, () => {
+  it(`should have as title 'best-quotes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('bestQuotes');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('best-quotes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('bestQuotes app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('best-quotes app is running!');
   });
 });
